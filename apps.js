8f173d9af6dde5a0692e23f53c9a214dff1ada04
awiles808 <awiles808@gmail.com>
2018-03-28 06:07:00 -0600

    Updated jS-doc.getTag

--- apps.js
+++ apps.js
@@ -1,6 +1,12 @@
 const form = document.getElementById("blogForm")
-form.addEventListener("Submit", function(event){event.preventDefault()} );
-const inputValue = document.getElementById("blogInput").value;
-let newElement = document.create("p");
-newElement.inner.HTML = input;
 
+form.addEventListener("Submit", function(event){
+    event.preventDefault()});
+
+let inputValue = document.getElementById("blogInput").value
+
+let newElement = document.create("p");
+
+newElement.inner.HTML = inputValue;
+
+document.getElementsByTagName("body")[0].appendChild(newElement);
