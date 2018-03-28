3b9b086d4c488c2fde4e3a48b00926099334f1ea
awiles808 <awiles808@gmail.com>
2018-03-27 20:57:06 -0600

    HTML and jS Started

---
+++ apps.js
@@ -0,0 +1,6 @@
+const form = document.getElementById("blogForm")
+form.addEventListener("Submit", function(event){event.preventDefault()} );
+const inputValue = document.getElementById("blogInput").value;
+let newElement = document.create("p");
+newElement.inner.HTML = input;
+
